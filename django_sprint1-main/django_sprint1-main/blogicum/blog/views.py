7503bd060bd951f92be42b005895c213c79f458b
Gundryul <gundrova2k18@gmail.com>
2024-12-03 10:39:32 +0300

    Add files via upload

---
+++ django_sprint1-main/django_sprint1-main/blogicum/blog/views.py
@@ -0,0 +1,73 @@
+posts = [
+    {
+        'id': 0,
+        'location': 'Остров отчаянья',
+        'date': '30 сентября 1659 года',
+        'category': 'travel',
+        'text': '''Наш корабль, застигнутый в открытом море
+                страшным штормом, потерпел крушение.
+                Весь экипаж, кроме меня, утонул; я же,
+                несчастный Робинзон Крузо, был выброшен
+                полумёртвым на берег этого проклятого острова,
+                который назвал островом Отчаяния.''',
+    },
+    {
+        'id': 1,
+        'location': 'Остров отчаянья',
+        'date': '1 октября 1659 года',
+        'category': 'not-my-day',
+        'text': '''Проснувшись поутру, я увидел, что наш корабль сняло
+                с мели приливом и пригнало гораздо ближе к берегу.
+                Это подало мне надежду, что, когда ветер стихнет,
+                мне удастся добраться до корабля и запастись едой и
+                другими необходимыми вещами. Я немного приободрился,
+                хотя печаль о погибших товарищах не покидала меня.
+                Мне всё думалось, что, останься мы на корабле, мы
+                непременно спаслись бы. Теперь из его обломков мы могли бы
+                построить баркас, на котором и выбрались бы из этого
+                гиблого места.''',
+    },
+    {
+        'id': 2,
+        'location': 'Остров отчаянья',
+        'date': '25 октября 1659 года',
+        'category': 'not-my-day',
+        'text': '''Всю ночь и весь день шёл дождь и дул сильный
+                порывистый ветер. 25 октября.  Корабль за ночь разбило
+                в щепки; на том месте, где он стоял, торчат какие-то
+                жалкие обломки,  да и те видны только во время отлива.
+                Весь этот день я хлопотал  около вещей: укрывал и
+                укутывал их, чтобы не испортились от дождя.''',
+    },
+]
+
+from django.shortcuts import render
+
+def index(request):
+    template_name = 'blog/index.html'
+    return render(request, template_name)
+
+def category_posts(request, category_slug):
+    template_name = 'blog/category.html'
+
+    category = category_slug
+
+    return render(request, template_name, context = {'category': category})
+
+def post_detail(request, id):
+    template_name = 'blog/detail.html'
+
+    first_post = posts[2]
+    second_post = posts[1]
+    third_post = posts[0]
+
+    my_posts = [first_post, second_post, third_post]
+
+    for i in range(len(my_posts)):
+        temp_val = first_post['id']
+        first_post['id'] = third_post['id']
+        third_post['id'] = temp_val
+
+    post = my_posts[id]
+
+    return render(request, template_name, context = {'post': post})
